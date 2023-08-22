e690b5cbb9937c308b4a0da2b48d393c1a12e162
Mark Guthrie <dumasymptote@gmail.com>
2023-08-22 08:51:11 -0500

    first setup from template

---
+++ README.md
@@ -0,0 +1,6 @@
+# This isnt really a bigdeal!
+
+
+
+### This was built using the eleventy base blog repo go check that out.
+https://github.com/11ty/eleventy-base-blog#readme
---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "dumasymptotedotcom",
+	url: "https://dumasymptote.com/",
+	language: "en",
+	description: "A blog with nothing actually useful.",
+	author: {
+		name: "MArk Guthrie",
+		email: "mguthriejr@gmail.com",
+		url: "https://dumasymptote.com/about/"
+	}
+}
