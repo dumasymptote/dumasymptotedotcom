64dde37349ac217bfe292357c06564dd99ea7e0d
Mark Guthrie <dumasymptote@gmail.com>
2023-08-22 21:06:44 -0500

    added second post and card page for indiv entry

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const { minify } = require("terser");
+
 const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
 
