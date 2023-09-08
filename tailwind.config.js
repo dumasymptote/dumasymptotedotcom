e238b300e3d4a25d540ad5df4e4bd4c32482e55e
Mark Guthrie <mguthriejr@gmail.com>
2023-09-07 20:43:06 -0500

    removed bulma and started adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '_site/**/*.html',
+    './_includes/**/*.njk',
+    './content/**/*.njk',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
