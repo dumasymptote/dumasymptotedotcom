c2355672512b0128bec838c5ef76df30b495602f
Mark Guthrie <dumasymptote@gmail.com>
2023-09-14 16:22:20 -0500

    menu working even if its not positioned correctly

--- _includes/scripts/main.js
+++ _includes/scripts/main.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-	// Get all "navbar-burger" elements
-	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-	// Add a click event on each of them
-	$navbarBurgers.forEach( el => {
-		el.addEventListener('click', () => {
-			// Get the target from the "data-target" attribute
-			const target = el.dataset.target;
-			const $target = document.getElementById(target);
-			// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-			el.classList.toggle('is-active');
-			$target.classList.toggle('is-active');
-			});
-	});
+	const navbarbutton = document.querySelector('#nav-button');
+  const navbaropen = document.querySelector('#nav-open');
+  const navbarclose = document.querySelector('#nav-close');
+  const navbar = document.querySelector('#navbar');
+  navbarbutton.addEventListener('click', (e) => {
+      navbaropen.classList.toggle('hidden');
+      navbarclose.classList.toggle('hidden');
+      navbar.classList.toggle('hidden');
+  });
 });
